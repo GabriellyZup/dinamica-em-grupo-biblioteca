62cac223a71a5f9d663663523514a0d019fa1297
joaocandidozup <joao.candido@zup.com>
2025-02-19 15:19:16 -0300

    feat: adiciona o endpoint cadastrar livro

--- src/main/java/br/com/biblioteca/controllers/LivroController.java
+++ src/main/java/br/com/biblioteca/controllers/LivroController.java
@@ -1,11 +1,24 @@
 package br.com.biblioteca.controllers;
 
+import br.com.biblioteca.dtos.LivroRequestDTO;
+import br.com.biblioteca.dtos.LivroResponseDTO;
+import br.com.biblioteca.services.LivroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
+    private LivroService livroService;
 
 
+    @PostMapping
+    public ResponseEntity<LivroResponseDTO> cradastrarLivro (@RequestBody LivroRequestDTO requestDTO){
+
+        return ResponseEntity.ok(livroService.cadastraLivro(requestDTO));
+    }
 }

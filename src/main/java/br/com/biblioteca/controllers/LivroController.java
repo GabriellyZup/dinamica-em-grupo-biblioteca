f2c2d1c3cebc13bd87ef0eb28388dc7ae84d83b2
joaocandidozup <joao.candido@zup.com>
2025-02-19 15:29:38 -0300

    refactor: classe controller

--- src/main/java/br/com/biblioteca/controllers/LivroController.java
+++ src/main/java/br/com/biblioteca/controllers/LivroController.java
@@ -21,7 +21,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
-    private LivroService livroService;
 
     private final LivroService livroService;
 

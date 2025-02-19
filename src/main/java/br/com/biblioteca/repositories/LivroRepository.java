c043927d3fa17440147fd47c4740e4a0dc011df3
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-19 14:39:42 -0300

    fix: adiciona o import mport br.com.biblioteca.models.LivroModel para classe LivroRepository.

--- src/main/java/br/com/biblioteca/repositories/LivroRepository.java
+++ src/main/java/br/com/biblioteca/repositories/LivroRepository.java
@@ -1,5 +1,6 @@
 package br.com.biblioteca.repositories;
 
+import br.com.biblioteca.models.LivroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<LivroModel, Long> {

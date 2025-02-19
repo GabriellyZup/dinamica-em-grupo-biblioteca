913c1b2e927dde32b594f91a5d92e06d035e9583
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-19 13:26:50 -0300

    refactor: design adjustment.

--- src/test/test/br/com/biblioteca/BibliotecaApplicationTests.java
+++ src/test/test/br/com/biblioteca/BibliotecaApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.dinamica_em_grupo_biblioteca.dinamica_em_grupo_biblioteca;
+package br.com.biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DinamicaEmGrupoBibliotecaApplicationTests {
+class BibliotecaApplicationTests {
 
 	@Test
 	void contextLoads() {

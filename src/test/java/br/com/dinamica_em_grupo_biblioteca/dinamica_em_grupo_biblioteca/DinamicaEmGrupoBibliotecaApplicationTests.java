8cec48213eae3d9b2009681bbeaea4efc138b921
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-19 11:57:46 -0300

    chore:add spring boot to the project.

---
+++ src/test/java/br/com/dinamica_em_grupo_biblioteca/dinamica_em_grupo_biblioteca/DinamicaEmGrupoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dinamica_em_grupo_biblioteca.dinamica_em_grupo_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinamicaEmGrupoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
